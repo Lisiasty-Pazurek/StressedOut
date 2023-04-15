ecc5ba24781d46935945d742f2c054a78b5a1efc
Lisiasty <sonicmephit@gmail.com>
2023-04-15 20:40:00 +0200

    UIController + sliders

--- Assets/Scritps/UIController.cs
+++ Assets/Scritps/UIController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
+    public Slider stressSlider;
+    public Slider progressSlider;
     // Start is called before the first frame update
     void Start()
     {
