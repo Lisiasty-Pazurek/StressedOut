adb54968d840c8c655c41790842c42d9820a59ef
Lisiasty <sonicmephit@gmail.com>
2023-04-16 21:53:49 +0200

    Basic follower for player controller

--- Assets/Scritps/LevelController.cs
+++ Assets/Scritps/LevelController.cs
@@ -7,15 +7,30 @@ public class LevelController : MonoBehaviour
     public float stressLevel;
     public float progressLevel;
 
-    // Start is called before the first frame update
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
+    }
+
+    private IEnumerator Heartbeat ()
+    {
+        for (int i = 0; i < 1; i++ )
+        {
+            Tick();
+        }
+        yield return null;
+    }
+
+    private void Tick()
+    {
+        progressLevel += .1f;
     }
 }
