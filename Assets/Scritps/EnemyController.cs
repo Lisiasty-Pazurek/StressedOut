744e52e8731c6dab452a9fa30bec70581ee8adbf
Lisiasty <sonicmephit@gmail.com>
2023-04-19 00:01:04 +0200

    Enemy and camera update for scaling

--- Assets/Scritps/EnemyController.cs
+++ Assets/Scritps/EnemyController.cs
@@ -6,15 +6,17 @@ public class EnemyController : MonoBehaviour
 {
     private Rigidbody2D rb;
     public CircleCollider2D eCollider;
+    public Vector2 moveDestination;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        moveDestination = new Vector2(Random.Range(0,3),Random.Range(0,5));
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.MovePosition(new Vector2(Random.Range(0,3),Random.Range(0,5)));
+        rb.AddForce(moveDestination);
     }
 }
