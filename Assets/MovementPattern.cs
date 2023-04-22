e65879e36b99d5a816d4fcdf031ffbc5d582a2de
Lugaid85 <124282593+Lugaid85@users.noreply.github.com>
2023-04-22 11:13:00 +0200

    Zmiany w Lvl01
    żeby nie było aż tak liniowo i mniej wrogów na wstęp

--- Assets/MovementPattern.cs
+++ Assets/MovementPattern.cs
@@ -6,18 +6,23 @@ public class MovementPattern : MonoBehaviour
 {
     public float xMin, xMax, yMin, yMax;
     public float x, y;
-    public float movementStep;
+    private int[] movementStep = new int[4];
+    
     public float speed;
     public PatternType patternType;
 
-    private Transform transform;
+    private Transform myTransform;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform = GetComponent<Transform>();
-        x=transform.position.x; 
-        y=transform.position.y;
+        myTransform = GetComponent<Transform>();
+        x=myTransform.position.x; 
+        y=myTransform.position.y;
+        movementStep[0] = 0;
+        movementStep[1] = 0;
+        movementStep[2] = 0;
+        movementStep[3] = 0;
     }
 
     // Update is called once per frame
@@ -31,6 +36,10 @@ public class MovementPattern : MonoBehaviour
             case PatternType.LeftRight:
                 LeftRightMovement();
                 break;
+            case PatternType.UpAndDownAndLeftRight:
+                UpAndDownMovement();
+                LeftRightMovement();
+                break;
             default:
                 break;
         }
@@ -38,47 +47,48 @@ public class MovementPattern : MonoBehaviour
 
     void UpAndDownMovement()
     {
-        if (movementStep == 0)
+        if (movementStep[0] == 0)
         {
             if (y >= yMax)
-                movementStep = 1;
+                movementStep[0] = 1;
             else
                 y += speed * Time.deltaTime;
-            transform.position = new Vector3(x, y, 0);
+            myTransform.position = new Vector3(x, y, 0);
         }
-        else if (movementStep == 1)
+        else if (movementStep[0] == 1)
         {
             if (y <= yMin)
-                movementStep = 0;
+                movementStep[0] = 0;
             else
                 y -= speed * Time.deltaTime;
-            transform.position = new Vector3(x, y, 0);
+            myTransform.position = new Vector3(x, y, 0);
         }
     }
 
     void LeftRightMovement()
     {
-        if (movementStep == 0)
+        if (movementStep[1] == 0)
         {
             if (x >= xMax)
-                movementStep = 1;
+                movementStep[1] = 1;
             else
                 x+=speed * Time.deltaTime;
-                transform.position = new Vector3(x, y, 0); 
+                myTransform.position = new Vector3(x, y, 0); 
         }
-        else if (movementStep == 1)
+        else if (movementStep[1] == 1)
         {
             if (x <= xMin)
-                movementStep = 0;
+                movementStep[1] = 0;
             else
                 x-=speed * Time.deltaTime;
-            transform.position = new Vector3(x, y, 0);
+            myTransform.position = new Vector3(x, y, 0);
         }
     }
 
     public enum PatternType
     {
         UpAndDown,
-        LeftRight
+        LeftRight,
+        UpAndDownAndLeftRight
     }
 }
